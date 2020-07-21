601924e921388b3b506f898d15cae31a619db7ec
srse <srse@srse.srse>
2020-07-21 18:29:09 +0200

    updated etcd url+version, added force-update option, fixed unpacking issues,fixed --name issue

--- scripts/controller/setup_etcd.sh
+++ scripts/controller/setup_etcd.sh
@@ -8,8 +8,9 @@ INTERNAL_IP=''
 CLUSTER_TOKEN=etcd-kubicluster
 CLIENT_PORT=2379
 PEER_PORT=2380
-ETCD_VERSION='3.3.5'
+ETCD_VERSION='3.4.10'
 INITIAL_CLUSTER=''
+FORCE_UPDATE=false
 # As long as there is at least one more argument, keep looping
 while [[ $# -gt 0 ]]; do
     key="$1"
@@ -32,6 +33,9 @@ while [[ $# -gt 0 ]]; do
         -ev=*|--etcd-version=*)
         ETCD_VERSION="${key#*=}"
         ;;
+        -f|--force-update)
+        FORCE_UPDATE=true
+        ;;
         -cmu=*|--cluster-member-uri=*)
         shift # past the key and to the value
         INITIAL_CLUSTER="${INITIAL_CLUSTER},$1"
@@ -49,37 +53,39 @@ done
 if [ "$(wget --help| grep 'command not found')" ]; then
   apt-get install -y wget
 fi
-if [ "$(tar --help| grep 'command not found')" ]; then 
+if [ "$(tar --help| grep 'command not found')" ]; then
   apt-get install -y tar
 fi
 
 CERTS_AND_CONFIGS_DIR=${NODE_WORK_DIR}/certs_and_configs
-ETC_DATA_DIR=${NODE_WORK_DIR}/etc_data
+ETCD_DATA_DIR=${NODE_WORK_DIR}/etcd_data
 
 # add this controller to the initial cluster list
 INITIAL_CLUSTER="$(hostname)=https://${INTERNAL_IP}:${PEER_PORT}${INITIAL_CLUSTER}"
 
-if [ -d /etc/etcd ]; then mkdir -p /etc/etcd; fi
-if [ -d /var/lib/etcd ]; then mkdir -p /var/lib/etcd; fi
-if [ -d ${NODE_WORK_DIR} ]; then mkdir -p ${NODE_WORK_DIR}; fi
+if [ ! -d /etc/etcd ]; then mkdir -p /etc/etcd; fi
+if [ ! -d /var/lib/etcd ]; then mkdir -p /var/lib/etcd; fi
+if [ ! -d ${NODE_WORK_DIR} ]; then mkdir -p ${NODE_WORK_DIR}; fi
 
-if [ -f ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz ]; then
+if [ ! -f ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz ]; then
+  if ! (dpkg -s ca-certificates); then apt-get install -y ca-certificates; fi
   wget -q --show-progress --https-only --timestamping \
-    "https://github.com/coreos/etcd/releases/download/v${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz" -O ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz
+    "https://github.com/etcd-io/etcd/releases/download/v${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz" -O ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz
 fi
 
-if [ -d ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION} ]; then
-  mkdir -p ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION}/
-  tar -xvf ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION}/
+if [ ! -d ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION} ]; then
+  tar -xvzf ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz -C ${NODE_WORK_DIR}/
+  mv ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION}-linux-amd64 ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION}
 fi
 
-if [ "$(systemctl status etcd.service | grep running)" == "" ]; then
+if [ "$(systemctl status etcd.service | grep running)" = "" ] || [ "${FORCE_UPDATE}" = true ]; then
   if [ -f /usr/local/bin/etcd ]; then rm -f /usr/local/bin/etcd; fi
   if [ -f /usr/local/bin/etcdctl ]; then rm -f /usr/local/bin/etcdctl; fi
   ln -s ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION}/etcd /usr/local/bin/etcd
   ln -s ${NODE_WORK_DIR}/etcd-v${ETCD_VERSION}/etcdctl /usr/local/bin/etcdctl
 
-  ETCD_NAME=etcd-$(hostname)
+  # TODO document why this needs to be equal to the hostname
+  ETCD_NAME=$(hostname)
 
   cat << EOF | tee /etc/systemd/system/etcd.service
 [Unit]
@@ -104,7 +110,7 @@ ExecStart=/usr/local/bin/etcd \\
   --initial-cluster-token ${CLUSTER_TOKEN} \\
   --initial-cluster ${INITIAL_CLUSTER} \\
   --initial-cluster-state new \\
-  --data-dir=${ETC_DATA_DIR}
+  --data-dir=${ETCD_DATA_DIR}
 Restart=on-failure
 RestartSec=5
 
@@ -113,6 +119,9 @@ WantedBy=multi-user.target
 EOF
   systemctl daemon-reload
   systemctl enable etcd
+  if [ "${FORCE_UPDATE}" = true ]; then
+    systemctl stop etcd
+  fi
   systemctl start etcd
   systemctl status etcd
 fi
@@ -127,9 +136,10 @@ else
     --cert=${CERTS_AND_CONFIGS_DIR}/kubernetes.pem \
     --key=${CERTS_AND_CONFIGS_DIR}/kubernetes-key.pem \
     | grep started)
-  if [ "${API_STARTED}" == "" ]; then
+  if [ "${API_STARTED}" = "" ]; then
     echo "installation of etcd v${ETCD_VERSION} failed. API did not start."
     exit 1
   else
     echo "installation of etcd v${ETCD_VERSION} successful."
+  fi
 fi

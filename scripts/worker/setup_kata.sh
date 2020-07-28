d0c507f32a345cd8db344b8c7350392f8c632d59
srse <srse@srse.srse>
2020-07-28 18:31:37 +0200

    improved setup kata script

--- scripts/worker/setup_kata.sh
+++ scripts/worker/setup_kata.sh
@@ -2,6 +2,19 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+KATA_VERSION='1.11.2'
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case "$key" in
+        -v=*|--version=*)
+        KATA_VERSION="${key#*=}"
+        ;;
+    esac
+    # Shift after checking all the cases to get the next option
+    shift
+done
+
 if ! which curl; then
   apt-get install -y curl
 fi
@@ -20,7 +33,7 @@ sysctl --system
 KERNEL_MODULES_FAILED=0
 for module in br_netfilter overlay; do
   if [ "$(grep ${module} /etc/modules)" = "" ]; then echo -e "\n${module}" >>/etc/modules ; modprobe ${module} ; fi
-  if ! lsmod | grep br_netfilter; then echo "kernel module ${module} could not be activated" ; KERNEL_MODULES_FAILED=$((${KERNEL_MODULES_FAILED} + 1)) ; fi
+  if ! lsmod | grep ${module}; then echo "kernel module ${module} could not be activated" ; KERNEL_MODULES_FAILED=$((${KERNEL_MODULES_FAILED} + 1)) ; fi
 done
 
 if [[ ${KERNEL_MODULES_FAILED} -gt 0 ]]; then exit ${KERNEL_MODULES_FAILED} ; fi
@@ -29,12 +42,23 @@ if [[ ${KERNEL_MODULES_FAILED} -gt 0 ]]; then exit ${KERNEL_MODULES_FAILED} ; fi
 # according to https://github.com/kata-containers/documentation/blob/master/install/debian-installation-guide.md
 export DEBIAN_FRONTEND=noninteractive
 ARCH=$(arch)
-BRANCH="${BRANCH:-master}"
-source /etc/os-release
-[ "$ID" = debian ] && [ -z "$VERSION_ID" ] && echo >&2 "ERROR: Debian unstable not supported.
+BRANCH='master'
+MAJOR_MINOR=''
+if [ "${KATA_VERSION}" != "master" ]; then
+  MAJOR_MINOR="$(echo ${KATA_VERSION} | cut -d '.' -f 1).$(echo ${KATA_VERSION} | cut -d '.' -f 2)"
+  BRANCH="stable-${MAJOR_MINOR}"
+  MAJOR_MINOR="=${MAJOR_MINOR}"
+fi
+if which kata-runtime && [ "$(kata-runtime --version | head -n 1 | cut -d ' ' -f 4 | cut -d '-' -f 1)" = "${KATA_VERSION}" ]; then
+  echo "kata ${KATA_VERSION} already exists"
+else
+  source /etc/os-release
+  [ "$ID" = debian ] && [ -z "$VERSION_ID" ] && echo >&2 "ERROR: Debian unstable not supported.
   You can try stable packages here:
   http://download.opensuse.org/repositories/home:/katacontainers:/releases:/${ARCH}:/${BRANCH}" && exit 1
-sh -c "echo 'deb http://download.opensuse.org/repositories/home:/katacontainers:/releases:/${ARCH}:/${BRANCH}/Debian_${VERSION_ID}/ /' > /etc/apt/sources.list.d/kata-containers.list"
-curl -sL  http://download.opensuse.org/repositories/home:/katacontainers:/releases:/${ARCH}:/${BRANCH}/Debian_${VERSION_ID}/Release.key | apt-key add -
-apt-get update
-apt-get -y install kata-runtime kata-proxy kata-shim
+  sh -c "echo 'deb http://download.opensuse.org/repositories/home:/katacontainers:/releases:/${ARCH}:/${BRANCH}/Debian_${VERSION_ID}/ /' > /etc/apt/sources.list.d/kata-containers.list"
+  curl -sL  http://download.opensuse.org/repositories/home:/katacontainers:/releases:/${ARCH}:/${BRANCH}/Debian_${VERSION_ID}/Release.key | apt-key add -
+  apt-get update
+  apt-get -y purge kata-* qemu-vanilla
+  apt-get -y install kata-runtime${MAJOR_MINOR}* kata-proxy${MAJOR_MINOR}* kata-shim${MAJOR_MINOR}*
+fi
